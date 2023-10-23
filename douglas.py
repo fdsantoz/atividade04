65c1a128dc06a608e9e38b5459df87ca38cff3f2
fdsantoz <147438578+fdsantoz@users.noreply.github.com>
2023-10-23 14:18:29 -0300

    Add files via upload

---
+++ douglas.py
@@ -0,0 +1,6 @@
+número = int(input("digite um número :"))
+antecessor = número - 1
+sucessor = número + 1
+print("o antecessor de",número, "e", antecessor)
+print("o sucessor de",número, "e", sucessor)
+print("PARABÉNS")
